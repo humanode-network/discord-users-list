30105c980791404aec8ddfbb52496e9fb46cbda6
MOZGIII <mike-n@narod.ru>
2023-05-15 18:28:27 -0300

    Initial setup

--- crates/discord-users-list/src/main.rs
+++ crates/discord-users-list/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+//! List discord users.
+
+#[tokio::main]
+async fn main() -> Result<(), color_eyre::eyre::Error> {
+    tracing_subscriber::fmt::init();
+    color_eyre::install()?;
+
+    let _discord_token: String = envfury::must("DISCORD_TOKEN")?;
+
+    Ok(())
 }
